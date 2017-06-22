e98cd795fc27dd993524a7bfc056b53efcaf9efe
AZName <964190389@qq.com>
2017-06-22 18:03:16 +0800

    feat: index

--- Home/views.py
+++ Home/views.py
@@ -3,8 +3,5 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.sessions.backends.db import SessionStore
 
 
-# Create your views here.
-
 def index(request):
-
     return render(request, "home/index.html")
